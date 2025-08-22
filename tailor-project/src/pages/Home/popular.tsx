a0735853acd28ac0b7002b7c1fbfe6aa951327c3
TenaGabriel <tenagabriel13@gmail.com>
2025-08-22 15:17:16 -0700

    made the popular slider buttons functional uisng useRef

--- tailor-project/src/pages/Home/popular.tsx
+++ tailor-project/src/pages/Home/popular.tsx
@@ -1,6 +1,22 @@
+import { useRef } from "react";
 import illustrationDetails from "../../utils";
 
 function Popular() {
+  const sliderRef = useRef<HTMLDivElement>(null)
+
+function nextButton() {
+    if (sliderRef.current) {
+      sliderRef.current.scrollLeft += 300; 
+    }
+  }
+
+
+    function prevButton() {
+    if (sliderRef.current) {
+      sliderRef.current.scrollLeft -= 300; 
+    }
+  }
+
   return (
     <>
       <h1 className="popular-category-text">See popular Categories.</h1>
@@ -9,11 +25,12 @@ function Popular() {
           className="prev-btn"
           src="images/icons/less-than-symbol.png"
           alt=""
+          onClick={prevButton}
         />
-        <div className="popular-slider">
+        <div className="popular-slider" ref={sliderRef}>
           {illustrationDetails.map((illustrationDetail) => {
             return (
-              <div className="popular-slide">
+              <div className="popular-slide" key={illustrationDetail.id}>
                 <img className="illustration-img" src={illustrationDetail.image} />
                 <p className="illustration-title">
                   ${illustrationDetail.imageTitle}
@@ -26,6 +43,7 @@ function Popular() {
           className="nxt-btn"
           src="images/icons/greater-than-symbol.png"
           alt=""
+          onClick={nextButton}
         />
       </div>
     </>
--- tailor-project/src/utils/index.ts
+++ tailor-project/src/utils/index.ts
@@ -1,37 +1,46 @@
 const illustrationDetails: {
   image: string,
-  imageTitle: string
+  imageTitle: string,
+  id: string
 }[] = [
   {
   image: "images/illustration-images/karen-willis-holmes-dOlFeKIDuRg-unsplash.jpg",
   imageTitle: "Wedding Guest Gown",
+  id: crypto.randomUUID()
 },
 {
   image: "images/illustration-images/female-red-gown.jpg",
-  imageTitle: "Plane women gown"
+  imageTitle: "Plane women gown",
+  id: crypto.randomUUID()
 },
  {
   image: "images/illustration-images/lucas-andrade-WSEn7t327uY-unsplash.jpg",
   imageTitle: "Halloween outfit",
+  id: crypto.randomUUID()
 },
 {
   image: "images/illustration-images/gym-men-one.jpg",
-  imageTitle: "Mens Gym outfit"
+  imageTitle: "Mens Gym outfit",
+  id: crypto.randomUUID()
 },
 {
   image: "images/illustration-images/trouser-suit.jpg",
-  imageTitle: "Ladies corporate"
+  imageTitle: "Ladies corporate",
+  id: crypto.randomUUID()
 },
  {
   image: "images/illustration-images/khalid-boutchich-wS1k0VzXVrA-unsplash.jpg",
   imageTitle: "Casual Date Night",
+  id: crypto.randomUUID()
 }, {
   image: "images/illustration-images/ankara-pant-one.jpg",
-  imageTitle: "Ankara pants"
+  imageTitle: "Ankara pants",
+  id: crypto.randomUUID()
 },
 {
   image: "images/illustration-images/beach-woman-two.jpg",
-  imageTitle: "womens beach oufit"
+  imageTitle: "womens beach oufit",
+  id: crypto.randomUUID()
 }
 ]
 
